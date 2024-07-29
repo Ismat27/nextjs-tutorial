c66a0a76d013bdf37bc3018ae40a6991214a7b8e
WEMABANK\Ismail.adegbite <Ismail.adegbite@wemabank.com>
2024-07-29 15:55:49 +0100

    streaming and data loading

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,8 +2,8 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
-import CardWrapper from '../ui/dashboard/cards';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '../../ui/skeletons';
+import CardWrapper from '../../ui/dashboard/cards';
 
 export default async function Page() {
     return (
